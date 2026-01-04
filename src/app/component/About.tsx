5ea98921e9c21c504b90ca2e673981db636a26f4
Toobailyas <137196562+Toobailyas@users.noreply.github.com>
2026-01-04 02:03:11 -0800

    Add C and Next.js to skills and update education list

--- src/app/component/About.tsx
+++ src/app/component/About.tsx
@@ -21,13 +21,16 @@ const About = () => {
                 <li>HTML</li>
                 <li>CSS</li>
                 <li>TypeScript</li>
+                    <li>C (Programming Lang)</li>
+                    <li>Next.js</li>
                 </div>
             </span>
             <span className="mr-3 text-xl font-bold hover:text-white text-[#ADB7BE] hover:border-b-2 border-purple-500">Education
             <div className="text-sm font-medium ">
                 <li>Matriculation from TCF(Al-Muhaimin campus) with Science</li>
                 <li>GIAIC Student (Artificial Intelligence, Web 3.0 & metaverse) <span className="text-purple-300">continue</span> </li>
-                <li>Intermediate from Khursheed Govt. college with Pre-Engineering <span className="text-purple-300">continue</span></li>
+                <li>Intermediate from Khursheed Govt. college with Pre-Engineering</li>
+                <li>FAST'29 (BAI)<span className="text-purple-300">continue</span></li>
                 </div>
             </span>
         </div>
